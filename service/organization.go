393efc33219ad3ef44d1974236a52529a6cfb0e0
Zinc4 <65228679+Zinc4@users.noreply.github.com>
2024-06-06 11:19:33 +0700

    improve create org feature admin

--- service/organization.go
+++ service/organization.go
@@ -1,13 +1,12 @@
 package service
 
 import (
-	"capstone/dto"
 	"capstone/entities"
 	"capstone/repositories"
 )
 
 type OrganizationService interface {
-	CreateOrganization(input dto.OrganizationRequest) (entities.Organization, error)
+	CreateOrganization(organization entities.Organization) (entities.Organization, error)
 	FindOrganizations() ([]entities.Organization, error)
 	FindOrganizationByID(id int) (entities.Organization, error)
 	FindFundraisingByOrganizationID(id int) ([]entities.Fundraising, error)
@@ -21,13 +20,7 @@ func NewOrganizationService(organizationRepository repositories.OrganizationRepo
 	return &organizationService{organizationRepository}
 }
 
-func (s *organizationService) CreateOrganization(input dto.OrganizationRequest) (entities.Organization, error) {
-	organization := entities.Organization{
-		Name:        input.Name,
-		Avatar:      input.Avatar,
-		Description: input.Description,
-		IsVerified:  input.IsVerified,
-	}
+func (s *organizationService) CreateOrganization(organization entities.Organization) (entities.Organization, error) {
 
 	return s.organizationRepository.Save(organization)
 }
