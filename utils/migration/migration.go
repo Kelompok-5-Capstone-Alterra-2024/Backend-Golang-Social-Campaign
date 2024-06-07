03c421c0a438b3b65c38e227934d134fec66546c
Zinc4 <65228679+Zinc4@users.noreply.github.com>
2024-06-07 08:19:47 +0700

    fix migration db #2

--- utils/migration/migration.go
+++ utils/migration/migration.go
@@ -9,36 +9,21 @@ import (
 
 func Migration() {
 	// List of all entities to be migrated
-	entitiesToMigrate := []interface{}{
-		&entities.User{},
-		&entities.Volunteer{},
-		&entities.Application{},
-		&entities.Article{},
-		&entities.Comment{},
-		&entities.LikesComment{},
-		&entities.TestimoniVolunteer{},
-		&entities.Fundraising{},
-		&entities.FundraisingCategory{},
-		&entities.Organization{},
-		&entities.Donation{},
-		&entities.DonationComment{},
-		&entities.LikeDonationComment{},
-	}
-
+	// s
 	if err := database.DB.AutoMigrate(&entities.User{}, &entities.Fundraising{}, &entities.FundraisingCategory{}, &entities.Organization{}, &entities.Donation{}, &entities.DonationComment{}, &entities.LikeDonationComment{}, &entities.Admin{}, &entities.Volunteer{}, &entities.Application{}, &entities.Article{}, &entities.Comment{}, &entities.LikesComment{}, &entities.TestimoniVolunteer{}); err != nil {
 		log.Fatal("Database migration failed")
 	}
 
 	// Loop through each entity and migrate
-	for _, entity := range entitiesToMigrate {
-		if database.DB.Migrator().HasTable(entity) == false {
-			database.DB.Migrator().CreateTable(entity)
-		}
+	// for _, entity := range entitiesToMigrate {
+	// 	if database.DB.Migrator().HasTable(entity) == false {
+	// 		database.DB.Migrator().CreateTable(entity)
+	// 	}
 
-		// if err := database.DB.AutoMigrate(entity); err != nil {
-		// 	log.Fatalf("Database migration failed for %T: %v", entity, err)
-		// }
+	// 	// if err := database.DB.AutoMigrate(entity); err != nil {
+	// 	// 	log.Fatalf("Database migration failed for %T: %v", entity, err)
+	// 	// }
 
-		fmt.Println("Successful database migration")
-	}
+	// }
+	fmt.Println("Successful database migration")
 }
