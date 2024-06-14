7ae5579c988b51631ede2723a64e7c00b006a8e5
Zinc4 <65228679+Zinc4@users.noreply.github.com>
2024-06-14 08:50:28 +0700

    fix route org volunterr & fundraising

--- routes/router.go
+++ routes/router.go
@@ -93,8 +93,8 @@ func NewRouter(router *echo.Echo) {
 	api.Use(jwt, routeMiddleware.UserMiddleware)
 
 	api.GET("/organizations", organizatonHandler.GetOrganizations)
-	api.GET("fundraising/organizations/:id", organizatonHandler.GetFundraisingsOrganizationByID)
-	api.GET("volunteer/organizations/:id", organizatonHandler.GetVolunteersByOrganizationByID)
+	api.GET("/fundraising/organizations/:id", organizatonHandler.GetFundraisingsOrganizationByID)
+	api.GET("/volunteer/organizations/:id", organizatonHandler.GetVolunteersByOrganizationByID)
 
 	api.GET("/fundraisings", fundraisingHandler.GetFundraisings)
 	api.GET("/fundraisings/top", fundraisingHandler.GetTopFundraisings)
