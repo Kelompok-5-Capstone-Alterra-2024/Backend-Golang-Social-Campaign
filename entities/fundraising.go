4e0cb78921726465c746a6a2148f571e18648317
Zinc4 <65228679+Zinc4@users.noreply.github.com>
2024-06-17 16:33:07 +0700

    fix transaction #4

--- entities/fundraising.go
+++ entities/fundraising.go
@@ -18,8 +18,8 @@ type Fundraising struct {
 	Status                string              `json:"status" gorm:"type:varchar(255)"`
 	GoalAmount            int                 `json:"goal_amount" gorm:"type:int"`
 	CurrentProgress       int                 `json:"current_progress" gorm:"type:int"`
-	StartDate             time.Time           `json:"start_date" gorm:"datetime"`
-	EndDate               time.Time           `json:"end_date" gorm:"datetime"`
+	StartDate             *time.Time          `json:"start_date" gorm:"datetime"`
+	EndDate               *time.Time          `json:"end_date" gorm:"datetime"`
 }
 
 type FundraisingCategory struct {
